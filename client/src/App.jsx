2c9c77362c708426c367c534baad1ba2c29c9647
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-01-26 15:26:26 -0800

    adding various files

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Signup from "./Signup";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/register"
+          element={<Signup />}
+        ></Route>
+        <Route
+          path="/login"
+          element={<Login />}
+        ></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
